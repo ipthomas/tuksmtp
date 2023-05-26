27b6aa4974dda78a180d5c7c0e930a3114fa4371
Ian <ian.thomas@tiani-spirit.com>
2023-05-26 18:42:04 +0100

    modified:   tuksmtp.go

--- tuksmtp.go
+++ tuksmtp.go
@@ -1,10 +1,8 @@
 package tuksmtp
 
 import (
-	"bytes"
 	"crypto/tls"
 	"fmt"
-	"html/template"
 	"log"
 	"net/smtp"
 
@@ -12,66 +10,81 @@ import (
 )
 
 type NotifyEvent struct {
-	Pathway       string
-	Expression    string
-	NHSId         string
-	User          string
-	Org           string
-	Role          string
 	Subject       string
+	Body          string
 	From          string
 	Server        string
 	Port          string
 	Password      string
 	Subscriptions tukdbint.Subscriptions
+	Event         tukdbint.Event
 }
-
-var emailTemplate = "{{define 'notifcation'}}Notification of {{.Pathway}} Workflow {{.Expression}} Event for NHS ID {{.NHSId}}\nEvent Created by User {{.User}} at Organisation {{.Org}} in the Role of {{.Role}}\n{{end}}"
 
 func (i *NotifyEvent) Notify() error {
 	var err error
-	var buf bytes.Buffer
-	var tmplt *template.Template
-	if tmplt, err = template.New("emailTemplate").Parse(emailTemplate); err == nil {
-		if err = tmplt.Execute(&buf, i); err == nil {
-			body := "ICB Workflow Event\n\n" + buf.String()
-			for _, v := range i.Subscriptions.Subscriptions {
-				if v.Email != "" {
-					emailBody := fmt.Sprintf("Subject: %s\r\n\r\n%s", i.Subject, body)
-					auth := smtp.PlainAuth("", i.From, i.Password, i.Server)
-					conn, err := smtp.Dial(i.Server + ":" + i.Port)
-					if err != nil {
-						log.Println(err.Error())
-						return err
-					}
-					defer conn.Close()
-					tlsConfig := &tls.Config{
-						InsecureSkipVerify: true,
-						ServerName:         i.Server,
-					}
-					if err = conn.StartTLS(tlsConfig); err == nil {
-						if err = conn.Auth(auth); err == nil {
-							if err = conn.Mail(i.From); err == nil {
-								if err = conn.Rcpt(v.Email); err == nil {
-									if wc, err := conn.Data(); err != nil {
-										log.Println(err.Error())
-									} else {
-										if _, err = fmt.Fprint(wc, emailBody); err == nil {
-											log.Printf("Notification sent to %s", v.Email)
-										}
-									}
-								}
-							}
-						}
-					}
-					if err != nil {
-						log.Println(err.Error())
+	body := "ICB Workflow Event\n\n" + i.Body
+	emailBody := fmt.Sprintf("Subject: %s\r\n\r\n%s", i.Subject, body)
+	auth := smtp.PlainAuth("", i.From, i.Password, i.Server)
+	conn, err := smtp.Dial(i.Server + ":" + i.Port)
+	if err != nil {
+		log.Println(err.Error())
+		return err
+	}
+	defer conn.Close()
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true,
+		ServerName:         i.Server,
+	}
+	if err = conn.StartTLS(tlsConfig); err != nil {
+		log.Println(err.Error())
+		return err
+	}
+	if err = conn.Auth(auth); err != nil {
+		log.Println(err.Error())
+		return err
+	}
+
+	for _, v := range i.Subscriptions.Subscriptions {
+		if i.shouldNotify(v) {
+			if err = conn.Mail(i.From); err != nil {
+				log.Println(err.Error())
+				continue
+			}
+			if err = conn.Rcpt(v.Email); err != nil {
+				log.Println(err.Error())
+				continue
+			}
+			wc, err := conn.Data()
+			if err != nil {
+				log.Println(err.Error())
+				continue
+			}
+			if _, err = fmt.Fprint(wc, emailBody); err != nil {
+				log.Println(err.Error())
+				continue
+			}
+			log.Printf("Notification sent to %s", v.Email)
+		}
+	}
+	return err
+}
+func (i *NotifyEvent) shouldNotify(sub tukdbint.Subscription) bool {
+	if sub.Email != "" {
+		if sub.Pathway == "" && sub.NhsId == "" && sub.Expression == "" {
+			return true
+		}
+		if sub.Pathway != "" {
+			if i.Pathway == sub.Pathway {
+				if sub.NhsId == "" && sub.Expression == "" {
+					return true
+				}
+				if sub.NhsId != "" {
+					if i.NHSId == sub.NhsId {
+
 					}
 				}
 			}
 		}
-	} else {
-		log.Println(err.Error())
 	}
-	return err
+	return false
 }
