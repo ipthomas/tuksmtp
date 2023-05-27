cb9342227e08869ac2d1d8b1ec50fdbd16f87eab
Ian <ian.thomas@tiani-spirit.com>
2023-05-27 17:24:33 +0100

    modified:   tuksmtp.go

--- tuksmtp.go
+++ tuksmtp.go
@@ -27,12 +27,13 @@ func (i *NotifyEvent) Notify() error {
 			body := "ICB Workflow Event\n\n" + i.Body
 			emailBody := fmt.Sprintf("Subject: %s\r\n\r\n%s", i.Subject, body)
 			auth := smtp.PlainAuth("", i.From, i.Password, i.Server)
+			log.Printf("Set Email Body\n%s", emailBody)
 			conn, err := smtp.Dial(i.Server + ":" + i.Port)
 			if err != nil {
 				log.Println(err.Error())
 				return err
 			}
-			log.Printf("Set Email Body\n%s", emailBody)
+			log.Printf("Connected to smtp server : %s", i.Server)
 			defer conn.Close()
 			tlsConfig := &tls.Config{
 				InsecureSkipVerify: true,
@@ -52,15 +53,18 @@ func (i *NotifyEvent) Notify() error {
 				log.Println(err.Error())
 				continue
 			}
+			log.Printf("Set Email from : %s", i.From)
 			if err = conn.Rcpt(v.Email); err != nil {
 				log.Println(err.Error())
 				continue
 			}
+			log.Printf("Set Email to : %s", v.Email)
 			wc, err := conn.Data()
 			if err != nil {
 				log.Println(err.Error())
 				continue
 			}
+			defer wc.Close()
 			if _, err = fmt.Fprint(wc, emailBody); err != nil {
 				log.Println(err.Error())
 			} else {
@@ -70,6 +74,7 @@ func (i *NotifyEvent) Notify() error {
 	}
 	return err
 }
+
 func (i *NotifyEvent) shouldNotify(sub tukdbint.Subscription) bool {
 	var shouldReturn bool
 	if sub.Email != "" {
@@ -90,3 +95,80 @@ func (i *NotifyEvent) shouldNotify(sub tukdbint.Subscription) bool {
 	log.Printf("Subscription Matched : %v", shouldReturn)
 	return shouldReturn
 }
+
+// package main
+
+// import (
+// 	"crypto/tls"
+// 	"fmt"
+// 	"net/smtp"
+// )
+
+// func SendEmail(from, to, subject, body string) error {
+// 	// SMTP server configuration
+// 	smtpServer := "smtp.gmail.com"
+// 	smtpPort := "587"
+// 	smtpUsername := "admin@tiani-spirit.co.uk"
+// 	smtpPassword := "cTjh_M7b7XWZa9_8qZMR"
+
+// 	// Compose the email
+// 	emailBody := fmt.Sprintf("Subject: %s\r\n\r\n%s", subject, body)
+
+// 	// Establish a connection to the SMTP server
+// 	auth := smtp.PlainAuth("", smtpUsername, smtpPassword, smtpServer)
+// 	conn, err := smtp.Dial(smtpServer + ":" + smtpPort)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer conn.Close()
+
+// 	// StartTLS to initiate a secure (encrypted) connection
+// 	tlsConfig := &tls.Config{
+// 		InsecureSkipVerify: true,
+// 		ServerName:         smtpServer,
+// 	}
+// 	if err := conn.StartTLS(tlsConfig); err != nil {
+// 		return err
+// 	}
+
+// 	// Authenticate
+// 	if err := conn.Auth(auth); err != nil {
+// 		return err
+// 	}
+
+// 	// Set the sender and recipient
+// 	if err := conn.Mail(from); err != nil {
+// 		return err
+// 	}
+// 	if err := conn.Rcpt(to); err != nil {
+// 		return err
+// 	}
+
+// 	// Send the email body
+// 	wc, err := conn.Data()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer wc.Close()
+
+// 	_, err = fmt.Fprint(wc, emailBody)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	return nil
+// }
+
+// func main() {
+// 	from := "admin@tiani-spirit.co.uk"
+// 	to := "ian.thomas@tiani-spirit.co.uk"
+// 	subject := "Hello, World!"
+// 	body := "This is the body of the email."
+
+// 	err := SendEmail(from, to, subject, body)
+// 	if err != nil {
+// 		fmt.Println("Error sending email:", err)
+// 	} else {
+// 		fmt.Println("Email sent successfully!")
+// 	}
+// }
