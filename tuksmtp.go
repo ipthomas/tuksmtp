8bd95181b2eae9f4e6408b604f403e392a8f5876
Ian <ian.thomas@tiani-spirit.com>
2023-05-30 14:58:55 +0100

    modified:   tuksmtp.go

--- tuksmtp.go
+++ tuksmtp.go
@@ -26,16 +26,17 @@ type NotifyEvent struct {
 func (i *NotifyEvent) Notify() error {
 	var err error
 	for _, v := range i.Subscriptions.Subscriptions {
-		if i.shouldNotify(v) {
+		if stmt, shouldNotify := i.shouldNotify(v); shouldNotify {
 			i.ConsumerURL = i.ConsumerURL + "?act=select&user=" + v.User + "&org=" + v.Org + "&role=" + v.Role + "&config=xdw&pathway=" + i.Event.Pathway + "&nhs=" + i.Event.NhsId + "&vers=" + tukutil.GetStringFromInt(i.Event.Version) + "&_format=html"
 			i.SubscriberURL = i.SubscriberURL + "?act=select&topic=EMAIL&user=" + v.User + "&org=" + v.Org + "&role=" + v.Role + "&email=" + v.Email + "&_format=html"
-			i.Body = "Notification of " + i.Event.Pathway + " Workflow Event\n\n" + i.Body
-			wfDetails := fmt.Sprintf("\r\n\r\nClick this link to view Workflow Details\r\n %s \r\n", i.ConsumerURL)
-			subDetails := fmt.Sprintf("\r\n\r\nClick this link to manage your Subscriptions to ICB Notifications\r\n %s \r\n", i.SubscriberURL)
+			i.Body = stmt + i.Body
+			wfurl := fmt.Sprintf("\r\n\r\nClick this link to view Workflow Details\r\n %s \r\n", i.ConsumerURL)
+			subsurl := fmt.Sprintf("\r\n\r\nClick this link to manage your Subscriptions to ICB Notifications\r\n %s \r\n", i.SubscriberURL)
 			emailBody := fmt.Sprintf("Subject: %s\r\n\r\n%s", i.Subject, i.Body)
-			emailBody = emailBody + wfDetails + subDetails
+			emailBody = emailBody + wfurl + subsurl
+			log.Printf("Set Email Content\n%s", emailBody)
+
 			auth := smtp.PlainAuth("", i.From, i.Password, i.Server)
-			log.Printf("Set Email Body\n%s", emailBody)
 			conn, err := smtp.Dial(i.Server + ":" + i.Port)
 			if err != nil {
 				log.Println(err.Error())
@@ -83,16 +84,20 @@ func (i *NotifyEvent) Notify() error {
 	return err
 }
 
-func (i *NotifyEvent) shouldNotify(sub tukdbint.Subscription) bool {
+func (i *NotifyEvent) shouldNotify(sub tukdbint.Subscription) (string, bool) {
+	var stmt = "You have subscribed to receive ICB Workflow Notifications"
 	var shouldReturn bool
 	if sub.Email != "" {
 		if sub.Pathway != "" {
 			shouldReturn = (i.Event.Pathway == sub.Pathway) && (sub.NhsId == "" || sub.NhsId == i.Event.NhsId) && (sub.Expression == "" || i.Event.Expression == sub.Expression)
+			stmt = stmt + " for " + sub.Pathway + " " + sub.NhsId + " " + sub.Expression + " Events"
 		} else {
 			if sub.NhsId != "" {
+				stmt = stmt + " for " + sub.NhsId + " " + sub.Expression + " Events"
 				shouldReturn = (i.Event.NhsId == sub.NhsId) && (sub.Expression == "" || i.Event.Expression == sub.Expression)
 			} else {
 				if sub.Expression != "" {
+					stmt = stmt + " for " + sub.Expression + " Events"
 					shouldReturn = i.Event.Expression == sub.Expression
 				} else {
 					shouldReturn = true
@@ -100,8 +105,9 @@ func (i *NotifyEvent) shouldNotify(sub tukdbint.Subscription) bool {
 			}
 		}
 	}
+	stmt = stmt + "\n\n"
 	log.Printf("Subscription Matched : %v", shouldReturn)
-	return shouldReturn
+	return stmt, shouldReturn
 }
 
 // package main
